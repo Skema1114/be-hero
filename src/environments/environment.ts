cf6e3e65247154ecfb7f7bef306a46e100742ce4
Rafael Aranda Martins <skema1114@hotmail.com>
2019-11-06 17:32:37 -0200

    Ajustando os Enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  fireConfig: {
+    apiKey: 'AIzaSyDmrDZpGMCZ6pFcoqOOMeyTmV8Q1s1vpG8',
+    authDomain: 'be-hero-pro.firebaseapp.com',
+    databaseURL: 'https://be-hero-pro.firebaseio.com',
+    projectId: 'be-hero-pro',
+    storageBucket: 'be-hero-pro.appspot.com',
+    messagingSenderId: '382314179468',
+    appId: '1:382314179468:web:9246798d182a47628804a9',
+    measurementId: 'G-CLN5RFXWLP'
+  }
 };
 
 /*
